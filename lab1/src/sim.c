cf350036bb49180caa2085200e23e0449e95d158
victor-a-a <jesusaa@uchicago.edu>
2020-10-15 17:38:21 -0500

    commented out all prints

--- lab1/src/sim.c
+++ lab1/src/sim.c
@@ -19,7 +19,7 @@ struct Decodes Decode_State = { .op = 0, .d = 0, .n = 0, .m = 0, .t = 0, .branch
 void fetch()
 {
     uint32_t word = mem_read_32(CURRENT_STATE.PC);
-    printf("%x\n",word);
+    //printf("%x\n",word);
     NEXT_STATE.PC = word;
 }
 void decode()
@@ -28,7 +28,7 @@ void decode()
   int temp = word & 0x1E000000;
   // Data Processing - Immediate
   if ((temp == 0x10000000) || (temp == 0x12000000)) {
-    printf("Data Processing - Immediate, ");
+    //printf("Data Processing - Immediate, ");
     int temp2 = word & 0x03800000;
     // Add/subtract (immediate)
     if ((temp2 == 0x01000000) || (temp2 == 0x01800000)) {
@@ -36,14 +36,14 @@ void decode()
       Decode_State.imm = (word & 0x003ffc00) >> 10;
       Decode_State.n = (word & 0x000003e0) >> 5;
       Decode_State.d = (word & 0x0000001f);
-      printf("Add/subtraction (immediate), ");
+      //printf("Add/subtraction (immediate), ");
     }
     // Move wide (immediate)
     else if (temp2 == 0x02800000) {
       Decode_State.op = (word & 0xff800000);
       Decode_State.imm = (word & 0x001fffe0) >> 5;
       Decode_State.d = (word & 0x0000001f);
-      printf("Move wide (immediate), ");
+      //printf("Move wide (immediate), ");
     }
     // Bitfield
     else if (temp2 == 0x03000000) {
@@ -53,16 +53,16 @@ void decode()
       Decode_State.d = (word & 0x0000001f);
       Decode_State.imm = (word & 0x0000fc00) >> 10;
       Decode_State.m = (word & 0x003f0000) >> 16; // using m as another imm
-      printf("Bitfield, ");
+      //printf("Bitfield, ");
     }
     else {
-      printf("Failure to match subtype");
+      //printf("Failure to match subtype");
     }
   }
 
   // Branches and Exceptions
   else if ((temp == 0x14000000) || (temp == 0x16000000)) {
-    printf("Branches and Exceptions, ");
+    //printf("Branches and Exceptions, ");
     // Conditional branch
     if ((word & 0xfe000000) == 0x54000000) {
       Decode_State.op = (word & 0xff000000);
@@ -70,36 +70,36 @@ void decode()
       Decode_State.d = (word & 0x0000000f);
       //Decode_State.imm = (word & 0x00ffffe0) >> 5;
       Decode_State.imm = ((word & 0x00FFFFE0) | ((word & 800000) ? 0xFFFFFFFFFFF80000 : 0));
-      printf("Conditional branch, ");
+      //printf("Conditional branch, ");
     }
     // Exception
     else if ((word & 0xff000000) == 0xd4000000) {
       Decode_State.op = (word & 0xffe00000);
       Decode_State.imm = (word & 0x001fffe0) >> 5;
-      printf("Exception, ");
+      //printf("Exception, ");
     }
     // Unconditional branch (register)
     else if ((word & 0xfe000000) == 0xd6000000) {
       Decode_State.op = (word & 0xfffffc00);
       Decode_State.n = (word & 0x000003e0) >> 5;
-      printf("Unconditional branch (register), ");
+      //printf("Unconditional branch (register), ");
     }
     // Unconditional branch (immediate)
     else if ((word & 0x60000000) == 0) {
       Decode_State.op = (word & 0xfc000000);
       //sign extending to 64 bits
       Decode_State.imm = (word & 0x03ffffff) | ((word & 0x2000000) ? 0xFFFFFFFFFC000000 : 0);
-      printf("Unconditional branch (immediate), ");
+      //printf("Unconditional branch (immediate), ");
     }
     // Compare and branch
     else if ((word & 0x7e000000) == 0x34000000) {
       Decode_State.op = (word & 0xff000000);
       Decode_State.t = (word & 0x0000001f);
       Decode_State.imm = (word & 0x00ffffe0) | ((word & 0x800000) ? 0xFFFFFFFFFF000000 : 0);
-      printf("Compare and branch, ");
+      //printf("Compare and branch, ");
     }
     else {
-      printf("Failure to match subtype");
+      //printf("Failure to match subtype");
     }
   }
   // Loads and Stores
@@ -109,11 +109,11 @@ void decode()
     Decode_State.d = (word & 0x0000001f);
     Decode_State.imm = (word & 0x001ff000) >> 12;
     Decode_State.m = word;
-    printf("Loads and Stores, Load/store (unscaled immediate), ");
+    //printf("Loads and Stores, Load/store (unscaled immediate), ");
   }
   // Data Processing - Register
   else if ((word & 0x0e000000) == 0x0a000000) {
-    printf("Data Processing - Register, ");
+    //printf("Data Processing - Register, ");
     // Logical (shifted register)
     if ((word & 0x1f000000) == 0x0a000000) {
       Decode_State.op = (word & 0xff000000);
@@ -122,7 +122,7 @@ void decode()
       Decode_State.d = (word & 0x0000001f);
       Decode_State.imm = (word & 0x0000fc00) >> 10;
       // Decode_State.shift = (word & 0x00c00000) >> 22; // i don't know how you guys would like me to store this - victor
-      printf("Logical (shifted register), ");
+      //printf("Logical (shifted register), ");
     }
     // Add/subtract (extended register)
     else if ((word & 0x1f200000) == 0x0b000000) {
@@ -130,7 +130,7 @@ void decode()
       Decode_State.m = (word & 0x001f0000) >> 16;
       Decode_State.n = (word & 0x000003e0) >> 5;
       Decode_State.d = (word & 0x0000001f);
-      printf("Add/subtract (extended register), ");
+      //printf("Add/subtract (extended register), ");
     }
     // Data processing (3 source)
     else if ((word & 0x1f000000) == 0x1b000000) {
@@ -138,14 +138,14 @@ void decode()
       Decode_State.m = (word & 0x001f0000) >> 16;
       Decode_State.n = (word & 0x000003e0) >> 5;
       Decode_State.d = (word & 0x0000001f);
-      printf("Data processing (3 source), ");
+      //printf("Data processing (3 source), ");
     }
     else {
-      printf("Failure to match subtype");
+      //printf("Failure to match subtype");
     }
   }
   else {
-    printf("Failure to match subtype");
+    //printf("Failure to match subtype");
   }
 }
 
@@ -570,130 +570,130 @@ void execute()
     {
         // Add/Subtract immediate
         case 0x91000000:
-          printf("ADD\n");
+          //printf("ADD\n");
           ADD_Immediate();
           break;
         case 0xb1000000:
-          printf("ADDS\n");
+          //printf("ADDS\n");
           ADDS_Immediate();
           break;
         case 0xd1000000:
-          printf("SUB\n");
+          //printf("SUB\n");
           SUB_Immediate();
           break;
         case 0xf1000000:
-          printf("SUBS\n");
+          //printf("SUBS\n");
           SUBS_Immediate();
           break;
         // Compare and branch
         case 0xb4000000:
-          printf("CBZ\n");
+          //printf("CBZ\n");
           CBZ();
           break;
         case 0xb5000000:
-          printf("CBNZ\n");
+          //printf("CBNZ\n");
           CBNZ();
           break;
         // Move wide
         case 0xd2800000:
-          printf("MOVZ\n");
+          //printf("MOVZ\n");
           MOVZ();
           break;
         // Bitfield
         case 0xd3000000:
-          printf("LSL or LSR\n"); //execution has to do the distinction
+          //printf("LSL or LSR\n"); //execution has to do the distinction
           BITSHIFT();
           break;
         // Conditional branch
         case 0x54000000:
-          printf("B.cond\n");
+          //printf("B.cond\n");
           B_Cond();
           break;
         // Exceptions
         case 0xd4400000:
-          printf("HLT\n");
+          //printf("HLT\n");
           HLT();
           break;
         // Unconditional branch (register)
         case 0xd61f0000:
-          printf("BR\n");
+          //printf("BR\n");
           BR();
           break;
         // Unconditional branch (immediate)
         case 0x14000000:
-          printf("B\n");
+          //printf("B\n");
           B();
           break;
         // Loads and Stores
         case 0xf8400000:
-          printf("LDUR\n");
+          //printf("LDUR\n");
           LDUR();
           break;
         case 0xb8400000:
-          printf("LDUR\n");
+          //printf("LDUR\n");
           LDUR2();
           break;
         case 0x38400000:
-          printf("LDURB\n");
+          //printf("LDURB\n");
           LDURB();
           break;
         case 0x78400000:
-          printf("LDURH\n");
+          //printf("LDURH\n");
           LDURH();
           break;
         case 0xf8000000:
-          printf("STUR\n");
+          //printf("STUR\n");
           STUR();
           break;
         case 0xb8000000:
-          printf("STUR\n");
+          //printf("STUR\n");
           STUR2();
           break;
         case 0x38000000:
-          printf("STURB\n");
+          //printf("STURB\n");
           STURB();
           break;
         case 0x78000000:
-          printf("STURH\n");
+          //printf("STURH\n");
           STURH();
           break;
         // Logical (shifted register)
         case 0x8a000000:
-          printf("AND\n");
+          //printf("AND\n");
           AND();
           break;
         case 0xea000000:
-          printf("ANDS\n");
+          //printf("ANDS\n");
           ANDS();
           break;
         case 0xca000000:
-          printf("EOR\n");
+          //printf("EOR\n");
           EOR();
           break;
         case 0xaa000000:
-          printf("ORR\n");
+          //printf("ORR\n");
           ORR();
           break;
         // Add/subtract (extended)
         case 0x8b200000:
-          printf("ADD\n");
+          //printf("ADD\n");
           ADD_Extended();
           break;
         case 0xab000000:
-          printf("ADDS\n");
+          //printf("ADDS\n");
           ADDS_Extended();
           break;
         case 0xcb000000:
-          printf("SUB\n");
+          //printf("SUB\n");
           SUB_Extended();
           break;
         case 0xeb000000:
-          printf("SUBS\n");
+          //printf("SUBS\n");
           SUBS_Extended();
           break;
         // Data Processing (3 source)
         case 0x9b000000:
-          printf("MUL\n");
+          //printf("MUL\n");
           MUL();
           break;
     }
@@ -703,7 +703,7 @@ void execute()
         NEXT_STATE.PC = CURRENT_STATE.PC + 4;
     }
     else {
-        Decode_State.branching = 0;    
+        Decode_State.branching = 0;
     }
 }
 
