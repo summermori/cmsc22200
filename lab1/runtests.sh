16f35b7f4f1e8ac9c1f5a6f88c90f59b6e375077
victor-a-a <jesusaa@uchicago.edu>
2020-10-15 18:44:02 -0500

    made readout clearer

--- lab1/runtests.sh
+++ lab1/runtests.sh
@@ -3,10 +3,10 @@
 echo "" > testresults.txt
 
 # converts all input files to hex
-for f in inputs/*.s
-do
-  ./inputs/asm2hex $f
-done
+# for f in inputs/*.s
+# do
+#   ./inputs/asm2hex $f
+# done
 
 # runs the simulators for each file
 for f in inputs/*.x
@@ -14,9 +14,10 @@ do
   echo "" > testsim.txt
   echo "" > testref.txt
   printf ">> Running ref on $f\n"
-  echo "go"; echo "rdum"; echo "mdum 0x10000000 0x10000100" | ./sim $f >> testref.txt
+  echo "go" echo "rdum" echo "mdum 0x10000000 0x10000100" | ./sim $f >> testref.txt
   printf ">> Running sim on $f\n"
-  echo "go"; echo "rdum"; echo "mdum 0x10000000 0x10000100" | ./src/sim $f >> testsim.txt
-  echo ">> $f\n" >> testresults.txt
+  echo "go" echo "rdum" echo "mdum 0x10000000 0x10000100" | ./src/sim $f >> testsim.txt
+  echo ">> $f" >> testresults.txt
   diff -s testref.txt testsim.txt >> testresults.txt
+  echo "" >> testresults.txt
 done
