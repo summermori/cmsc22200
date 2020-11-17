7860673bda51322e87bb0e50751644157c061ed0
victor-a-a <jesusaa@uchicago.edu>
2020-11-17 16:43:38 -0600

    nested loop 1

--- lab3/inputs/nestedloop1.s
+++ lab3/inputs/nestedloop1.s
@@ -1,18 +1,27 @@
-WIP
-
 .text
-mov X1, 1000
+mov X1, 100
 mov X2, 0
+mov X10, 10
+mov X11, 0
 
 foo:
 mov X3, 1
 mov X3, 1
 mov X3, 1
+b bar
+
+fu:
+mov X11, 0
 add X2, X2, 1
 cmp X2, X1
+beq baz
 bne foo
 
 bar:
+add X11, X11, 1
+cmp X11, X10
+beq fu
+bne bar
 
 baz:
 mov X1, 4
