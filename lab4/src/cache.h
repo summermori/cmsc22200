aa9ce64e209f9b3966662f0fb1d503a2e38556a1
victor-a-a <jesusaa@uchicago.edu>
2020-11-27 13:46:24 -0600

    pushing changes

--- lab4/src/cache.h
+++ lab4/src/cache.h
@@ -14,8 +14,23 @@ typedef struct
 
 } cache_t;
 
-cache_t *cache_new(int sets, int ways, int block);
-void cache_destroy(cache_t *c);
-int cache_update(cache_t *c, uint64_t addr);
+typedef struct
+{
+
+} block_t;
+
+extern cache_t INST_CACHE;
+extern cache_t DATA_CACHE;
+
+// IGNORE THESE FOR NOW
+
+// void cache_destroy(cache_t *c); // dealocates
+// int cache_update(cache_t *c, uint64_t addr);
+
+cache_t cache_new(int sets, int ways, int block); // allocates
+
+uint_32 cache_read(uint_32 addr, uint_32int n);
+
+void cache_write (uint_32 addr, uint_32 val);
 
 #endif
