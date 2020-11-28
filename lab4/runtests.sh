25c2a05ff9b8030da9e74877486a2077f38da834
Samuel Hayes Sagawa <ssagawa@linux4.cs.uchicago.edu>
2020-11-27 20:48:38 -0600

    testing file

---
+++ lab4/runtests.sh
@@ -0,0 +1,25 @@
+echo "" > tresults.txt
+
+# converts all input files to hex
+for f in inputs/*.s
+do
+  ./inputs/asm2hex $f
+done
+
+# runs the simulators for each file
+for f in inputs/*.x
+do
+  echo "" > testsim.txt
+  echo "" > testref.txt
+  printf ">> Running ref on $f\n"
+  for i in {1..110};
+  do
+  	echo "run 1" echo "rdump"; done | ./src/refsim $f >> testref.txt
+  printf ">> Running sim on $f\n"
+  for i in {1..110};
+  do
+  	echo "run 1" echo "rdump"; done | ./src/sim $f >> testsim.txt
+  echo ">> $f" >> tresults.txt
+  diff -s testref.txt testsim.txt >> tresults.txt
+  echo "" >> tresults.txt
+done
